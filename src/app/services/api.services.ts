981f99b0519239471f6f932995a6e668577399bc
George Almonte <georgeaf1825@gmail.com>
2019-06-12 10:06:12 -0400

    added location

--- src/app/services/api.services.ts
+++ src/app/services/api.services.ts
@@ -7,16 +7,16 @@ export class Api {
     server = 'https://thingproxy.freeboard.io/fetch/'
     apiUrl = 'https://api.darksky.net/forecast';
     apiKey = 'a37531bbb850d56fe736c132b318ead7';
-    lat = '42.3601';
-    long = '-71.0589';
-    // https://api.darksky.net/forecast/a37531bbb850d56fe736c132b318ead7/42.3601,-71.0589
+    // lat = '42.3314';
+    // long = '-83.0458';
+    // https://api.darksky.net/forecast/a37531bbb850d56fe736c132b318ead7/42.3314,-83.0458
 
     constructor(private http: HttpClient) {
 
     }
 
-    getWeather = () => {
-       return this.http.get(`${this.server}${this.apiUrl}/${this.apiKey}/${this.lat},${this.long}`)
+    getWeather = (lat, long) => {
+       return this.http.get(`${this.server}${this.apiUrl}/${this.apiKey}/${lat},${long}`)
 
     }
 
