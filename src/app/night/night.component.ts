52586f596dd1246b74a98387a82eb17a372c44ff
hbruce48226 <holdenb5@gmail.com>
2019-06-19 00:15:34 -0400

    final correction

--- src/app/night/night.component.ts
+++ src/app/night/night.component.ts
@@ -23,59 +23,59 @@ export class NightComponent {
     pictureUrl = '../../assets/images/moon-cloud.png';
 
     timeString: string = '';
-    // callDate = () => {
-    //     let now = new Date();
-    //     this.dateString = now; 
-    //     console.log(`now: ${now}`);
-    // }
     callDate = () => {
         let now = new Date();
-        this.dateString = now.toISOString();
-        console.log(this.dateString);
-        let i;
-        for (i = 0; i < 10; i++) {
-            this.dateStringShort += this.dateString[i];
-        };
-        console.log(`this.dateStringShort: ${this.dateStringShort}`);
-        console.log(`this.dateStringShort[-1]: ${this.dateStringShort[this.dateStringShort.length - 1]}`)
-
-        let timeStringShort: string = '';
-        for (i = 11; i < 13; i++) {
-            this.timeString = now.toISOString();
-            timeStringShort += this.timeString[i];
-
-            console.log(`this.timeString[i]: ${this.timeString[i]}`);
-        }
-        console.log(`this.timeString: ${this.timeString}`);
-        console.log(`timeStringShort: ${timeStringShort}`);
-        let timeStringNumber = parseInt(timeStringShort);
-        if (timeStringNumber >= 20 || timeStringNumber <= 4) {
-            let newTimeString: string = '';
-            let lastNumberOfDate = parseInt(this.dateStringShort[this.dateStringShort.length - 1]);
-            let changeLastNumberOfDate = lastNumberOfDate - 1;
-            changeLastNumberOfDate.toString();
-            for (i = 0; i < this.dateStringShort.length - 1; i++) {
-                newTimeString += this.dateStringShort[i];
-                console.log(newTimeString);
-            }
-            newTimeString += changeLastNumberOfDate;
-            newTimeString += 'T00:00:00';
-            console.log(`newTimeString: ${newTimeString}`);
-            this.dateStringShort = newTimeString;
-
-
-            // let changeLastNumber = parseInt(this.dateStringShort[this.dateStringShort.length-1]);
-            // console.log(`changeLastNumber: ${changeLastNumber}`);
-            // changeLastNumber -= 1;
-            // console.log(`changeLastNumber: ${changeLastNumber}`);
-            // changeLastNumber.toString();
-            // this.dateStringShort = this.dateStringShort.slice(changeLastNumber,this.dateStringShort.length-1);
-        }
-
-        console.log(`this.dateStringShort: ${this.dateStringShort}`);
-        console.log(typeof this.dateStringShort);
-
+        this.dateString = now; 
+        console.log(`now: ${now}`);
     }
+    // callDate = () => {
+    //     let now = new Date();
+    //     this.dateString = now.toISOString();
+    //     console.log(this.dateString);
+    //     let i;
+    //     for (i = 0; i < 10; i++) {
+    //         this.dateStringShort += this.dateString[i];
+    //     };
+    //     console.log(`this.dateStringShort: ${this.dateStringShort}`);
+    //     console.log(`this.dateStringShort[-1]: ${this.dateStringShort[this.dateStringShort.length - 1]}`)
+
+    //     let timeStringShort: string = '';
+    //     for (i = 11; i < 13; i++) {
+    //         this.timeString = now.toISOString();
+    //         timeStringShort += this.timeString[i];
+
+    //         console.log(`this.timeString[i]: ${this.timeString[i]}`);
+    //     }
+    //     console.log(`this.timeString: ${this.timeString}`);
+    //     console.log(`timeStringShort: ${timeStringShort}`);
+    //     let timeStringNumber = parseInt(timeStringShort);
+    //     if (timeStringNumber >= 20 || timeStringNumber <= 4) {
+    //         let newTimeString: string = '';
+    //         let lastNumberOfDate = parseInt(this.dateStringShort[this.dateStringShort.length - 1]);
+    //         let changeLastNumberOfDate = lastNumberOfDate - 1;
+    //         changeLastNumberOfDate.toString();
+    //         for (i = 0; i < this.dateStringShort.length - 1; i++) {
+    //             newTimeString += this.dateStringShort[i];
+    //             console.log(newTimeString);
+    //         }
+    //         newTimeString += changeLastNumberOfDate;
+    //         newTimeString += 'T00:00:00';
+    //         console.log(`newTimeString: ${newTimeString}`);
+    //         this.dateStringShort = newTimeString;
+
+
+    //         // let changeLastNumber = parseInt(this.dateStringShort[this.dateStringShort.length-1]);
+    //         // console.log(`changeLastNumber: ${changeLastNumber}`);
+    //         // changeLastNumber -= 1;
+    //         // console.log(`changeLastNumber: ${changeLastNumber}`);
+    //         // changeLastNumber.toString();
+    //         // this.dateStringShort = this.dateStringShort.slice(changeLastNumber,this.dateStringShort.length-1);
+    //     }
+
+    //     console.log(`this.dateStringShort: ${this.dateStringShort}`);
+    //     console.log(typeof this.dateStringShort);
+
+    // }
 
 
     @Input() lowTemp: number;
